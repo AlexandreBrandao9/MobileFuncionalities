ff45100f914255e65520900369ef9a529bffbfaa
AlexandreBrandao9 <alexbr.2104@gmail.com>
2023-05-29 18:34:53 +0100

    msg

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ const shareData = {
   const btn = document.querySelector("#button-share");
   const resultPara = document.querySelector("#result");
   
-  // Share must be triggered by "user activation"
   btn.addEventListener("click", async () => {
     try {
       await navigator.share(shareData);
@@ -23,43 +22,6 @@ const shareData = {
     }
   });
 
-
-
-
-
-
-
-//   // Put variables in global scope to make them available to the browser console.
-// const video = document.querySelector("#video");
-// const constraints = {
-//   audio: false,
-//   video: true,
-// };
-
-// navigator.mediaDevices
-//   .getUserMedia(constraints)
-//   .then((stream) => {
-//     const videoTracks = stream.getVideoTracks();
-//     console.log("Got stream with constraints:", constraints);
-//     console.log(`Using video device: ${videoTracks[0].label}`);
-//     stream.onremovetrack = () => {
-//       console.log("Stream ended");
-//     };
-//     video.srcObject = stream;
-//   })
-//   .catch((error) => {
-//     if (error.name === "ConstraintNotSatisfiedError") {
-//       console.error(
-//         `The resolution ${constraints.video.width.exact}x${constraints.video.height.exact} px is not supported by your device.`
-//       );
-//     } else if (error.name === "PermissionDeniedError") {
-//       console.error(
-//         "You need to grant this page permission to access your camera and microphone."
-//       );
-//     } else {
-//       console.error(`getUserMedia error: ${error.name}`, error);
-//     }
-//   });
 
 
 
@@ -86,8 +48,7 @@ function error(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
 }
   
-  
-  
+
 
 
 
